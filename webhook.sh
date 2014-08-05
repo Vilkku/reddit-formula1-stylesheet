6eb39bd6431d81905bf40fd8d1dfec11da2a22f9
Vilkku <Vilkku@users.noreply.github.com>
2014-08-05 23:16:59 +0300

    Update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git pull && ~/python27/bin/python -mrcssmin <style.css >style.min.css && ~/python27/bin/python webhook.py
+cd ~/reddit-formula1-stylesheet && git pull && ~/python27/bin/python -mrcssmin <style.css >style.min.css && ~/python27/bin/python webhook.py
