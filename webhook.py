1babf40d047f50f7a77dbb88fa0e94c43a8c0a50
Vilkku <Vilkku@users.noreply.github.com>
2014-08-06 00:01:41 +0300

    Update webhook.py
    Made it work with /r/formula1

--- webhook.py
+++ webhook.py
@@ -15,6 +15,6 @@ r.login(config['reddit']['username'], config['reddit']['password'])
 # Update the stylesheet
 dir = path.dirname(__file__)
 stylesheet = open(path.join(dir, "style.min.css"), "r")
-r.get_subreddit("vilkku").set_stylesheet(stylesheet)
+r.get_subreddit("formula1").set_stylesheet(stylesheet)
 
 stylesheet.close()
