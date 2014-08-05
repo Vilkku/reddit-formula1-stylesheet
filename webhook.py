7506f603a00571a73735ad6d8eb7341b5d6905b0
Wilhelm Törnroos <vilkku@gmail.com>
2014-08-05 18:14:28 +0300

    Added stuff to wehbook.py

---
+++ webhook.py
@@ -0,0 +1,20 @@
+import praw
+import json
+from os import path
+
+# Read configuration file
+dir = path.dirname(__file__)
+config_data = open(path.join(dir, "config.json"), "r")
+config = json.load(config_data)
+config_data.close()
+
+# Reddit login
+r = praw.Reddit(config['reddit']['user_agent'])
+r.login(config['reddit']['username'], config['reddit']['password'])
+
+# Update the stylesheet
+dir = path.dirname(__file__)
+stylesheet = open(path.join(dir, "style.min.css"), "r")
+r.get_subreddit("vilkku").set_stylesheet(stylesheet)
+
+stylesheet.close()
