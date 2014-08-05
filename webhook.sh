db2cfa6520930e20cf32435d2c81673fc1ddc2df
Vilkku <Vilkku@users.noreply.github.com>
2014-08-05 19:50:57 +0300

    Update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-git pull
-~/python27/bin/python -mrcssmin <style.css >style.min.css
-~/python27/bin/python webhook.py
+git pull && ~/python27/bin/python -mrcssmin <style.css >style.min.css && ~/python27/bin/python webhook.py
