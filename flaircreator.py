318940bee4501db44e2dab9d9c0eed3f41215c97
Wilhelm Tornroos <vilkku@gmail.com>
2015-01-01 20:53:04 +0200

    Initial import of flair management tools

---
+++ flaircreator.py
@@ -0,0 +1,38 @@
+from os import listdir, path
+from PIL import Image
+import subprocess
+import sys
+
+flair_directory = path.join(path.dirname(__file__), 'flair')
+sheet_output = 'sheet.png'
+css_output = 'flair.css'
+
+try:
+    subprocess.check_call(['montage', path.join(flair_directory, '*'), '-tile', '1x', '-geometry', '20x16+1+1', '-background', 'none', 'sheet.png'])
+except subprocess.CalledProcessError:
+    pass
+except OSError:
+    sys.exit('Error calling ImageMagick montage')
+
+try:
+    subprocess.check_call(['optipng', sheet_output])
+except subprocess.CalledProcessError:
+    pass
+except OSError:
+    print 'Error calling OptiPNG, output has not been optimized'
+
+css_flair = []
+vpos = 1
+
+for i, name in enumerate(listdir(flair_directory)):
+    im = Image.open(path.join(flair_directory, name))
+    width, height = im.size
+    name = name.split('.')
+    name = name[0].replace(' ', '').replace('-', '')
+    css_flair.append('.flair-%(name)s:before,a[href="/%(name)s"]:before{background-position:-1px -%(vpos)dpx}' % {'name': name, 'vpos': vpos})
+    # 2 is the padding around the image which is set in the montage geometry option
+    vpos = vpos + (height+2)
+
+fo = open(css_output, 'w')
+fo.write('\n'.join(css_flair))
+fo.close()
---
+++ flairsync.py
@@ -0,0 +1,49 @@
+import csv
+import praw
+import json
+from os import path
+
+def templates_from_csv(path):
+    f = csv.reader(file(path))
+    # skip header row
+    f.next()
+    return [(r[0], r[1], csv_bool(r[2])) for r in f]
+
+def csv_bool(value):
+    # interpret a string as a boolean in the same way that the server does for
+    # CGI parameters
+    value = value.lower()
+    if not value or value[0] in 'fn' or value == 'off':
+        return False
+    return True
+
+# Read configuration file
+dir = path.dirname(__file__)
+config_data = open(path.join(dir, "config.json"), "r")
+config = json.load(config_data)
+config_data.close()
+
+r = praw.Reddit(config['reddit']['user_agent'])
+r.login(config['reddit']['username'], config['reddit']['password'])
+subreddit = 'formula1'
+
+print 'Parsing csv file ...'
+csv_templates = templates_from_csv('flair.csv')
+
+print 'Clearing flair templates ...'
+r.get_subreddit(subreddit).clear_flair_templates()
+
+for text, css_class, text_editable in csv_templates:
+    print 'Adding flair templates: %r, %r, %r' % (
+        text, css_class, text_editable)
+    count = 0
+    while count < 3:
+        try:
+            r.get_subreddit(subreddit).add_flair_template(text=text, css_class=css_class)
+
+        except:
+            count += 1
+        else:
+            break
+
+print 'Done!'
